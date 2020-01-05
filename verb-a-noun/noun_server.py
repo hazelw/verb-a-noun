8b3e7db1c35c490d51538abe9c542e36991c6923
Hazel Wright <hazel.wright@live.co.uk>
2020-01-05 23:42:56 +0000

    feat: actually working :tada:

--- verb-a-noun/noun_server.py
+++ verb-a-noun/noun_server.py
@@ -1,9 +1,22 @@
+from concurrent import futures
 import random
+import grpc
 
-from . import noun_pb2, noun_pb2_grpc
+import noun_pb2, noun_pb2_grpc
 
 
 class Noun(noun_pb2_grpc.NounServicer):
     def ProvideNoun(self, request, context):
         message = random.choice(['train', 'plane', 'car', 'bus', 'scooter'])
-        return noun_pb2.NounResponse(message=message)
+        return noun_pb2.NounResponse(noun=message)
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
+    noun_pb2_grpc.add_NounServicer_to_server(Noun(), server)
+    server.add_insecure_port('[::]:50052')
+    server.start()
+    server.wait_for_termination()
+
+
+serve()
--- verb-a-noun/verb_server.py
+++ verb-a-noun/verb_server.py
@@ -1,9 +1,22 @@
+from concurrent import futures
 import random
+import grpc
 
-from . import verb_pb2, verb_pb2_grpc
+import verb_pb2, verb_pb2_grpc
 
 
-class Verb(noun_pb2_grpc.VerbServicer):
+class Verb(verb_pb2_grpc.VerbServicer):
     def ProvideVerb(self, request, context):
         message = random.choice(['drive', 'fly', 'scoot'])
-        return verb_pb2.VerbResponse(message=message)
+        return verb_pb2.VerbResponse(verb=message)
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
+    verb_pb2_grpc.add_VerbServicer_to_server(Verb(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    server.wait_for_termination()
+
+
+serve()
